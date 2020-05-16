922a88ddca2060569516aa1ef109f5bd9ca4b0bb
melvinioso <matt.mcgavern@gmail.com>
2020-05-15 21:20:22 -0500

    Proxy refactor

--- index.js
+++ index.js
@@ -1,125 +1,64 @@
 const express = require('express');
 const axios = require('axios');
-const bodyParser = require('body-parser');
 
 const app = express();
-const jsonParser = bodyParser.json();
-const PORT = 3121;
-//const REVIEWS = 'http://localhost:3002/';
-const REVIEWS = 'http://3.22.26.129/';
-const CART = 'http://18.221.207.9:3001/';
-//const CART = 'http://localhost:3001/';
-const CAROUSEL = 'http://3.18.37.38:3003/';
-//const CAROUSEL = 'http://localhost:3003/';
-const DETAILS = 'http://54.69.127.206:3004/';
-//const DETAILS = 'http://localhost:3004/';
-const RECOMMENDED = 'http://3.18.37.38:3005/';
-//const RECOMMENDED = 'http://localhost:3005/';
+const PORT = 3100;
 
-app.listen(PORT, () => { console.log(`Now listening on port ${PORT}`); });
+app.use('/games/:gameId', express.static(`${__dirname}/../public`));
+app.use(express.json());
 
-app.use('/games/:gameId', express.static('public'));
 
-app.get('/', (req, res) => {
-  res.redirect('/games/1');
+app.get('/carousel/:gameId', (req, res) => {
+  axios({
+    baseURL: `http://3.18.37.38:3003/carousel/${req.params.gameId}`
+  })
+    .then((data) => { res.status(200).send(data.data); })
+    .catch(() => { res.status(404).end(); });
 });
 
-app.all('/reviews/:gameId', jsonParser, (req, res) => {
-  const METHOD = req.method;
-  const BODY = req.body;
-  axios({
-    method: METHOD,
-    url: `${REVIEWS}reviews/${req.params.gameId}`,
-    data: JSON.stringify(BODY),
-    headers: {
-      'Content-Type': 'application/json',
-    },
+app.get('/recommended', (req, res) => {
+  axios.get('/recommended', {
+    baseURL: 'http://3.18.37.38:3005',
   })
-    .then((response) => {
-      res.status(200).send(response.data);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).send('Something went wrong!');
-    });
+    .then((data) => { res.status(200).send(data.data); })
+    .catch(() => { res.status(404).end(); });
 });
 
-app.all('/cartapi/:gameId', jsonParser, (req, res) => {
-  const METHOD = req.method;
-  const BODY = req.body;
+app.get('/reviews/:gameId', (req, res) => {
   axios({
-    method: METHOD,
-    url: `${CART}cartapi/${req.params.gameId}`,
-    data: JSON.stringify(BODY),
-    headers: {
-      'Content-Type': 'application/json',
-    },
+    baseURL: `http://3.22.26.129/reviews/${req.params.gameId}`
   })
-    .then((response) => {
-      res.status(200).send(response.data);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).send('Something went wrong!');
-    });
+    .then((data) => { res.status(200).send(data.data); })
+    .catch(() => { res.status(404).end(); });
 });
 
-app.all('/carousel/:gameId', jsonParser, (req, res) => {
-  const METHOD = req.method;
-  const BODY = req.body;
+app.post('/reviews/:gameId', (req, res) => {
   axios({
-    method: METHOD,
-    url: `${CAROUSEL}carousel/${req.params.gameId}`,
-    data: JSON.stringify(BODY),
-    headers: {
-      'Content-Type': 'application/json',
-    },
+    method: 'post',
+    baseURL: `http://3.22.26.129/reviews/${req.params.gameId}`,
+    data: req.body,
   })
-    .then((response) => {
-      res.status(200).send(response.data);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).send('Something went wrong!');
-    });
+    .then(() => { res.status(200).send(); })
+    .catch(() => { res.status(404).end(); });
 });
 
-app.all('/recommended/', jsonParser, (req, res) => {
-  const METHOD = req.method;
-  const BODY = req.body;
+app.get('/cartapi/:gameId', (req, res) => {
   axios({
-    method: METHOD,
-    url: `${RECOMMENDED}recommended/`,
-    data: JSON.stringify(BODY),
-    headers: {
-      'Content-Type': 'application/json',
-    },
+    baseURL: `http://18.221.207.9:3001/cartapi/${req.params.gameId}`
   })
-    .then((response) => {
-      res.status(200).send(response.data);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).send('Something went wrong!');
-    });
+    .then((data) => { res.status(200).send(data.data); })
+    .catch(() => { res.status(404).end(); });
 });
 
-app.all('/api/games/:gameId', jsonParser, (req, res) => {
-  const METHOD = req.method;
-  const BODY = req.body;
+app.get('/api/games/:gameId', (req, res) => {
   axios({
-    method: METHOD,
-    url: `${DETAILS}api/games/${req.params.gameId}`,
-    data: JSON.stringify(BODY),
-    headers: {
-      'Content-Type': 'application/json',
-    },
+    baseURL: `http://54.69.127.206:3004/api/games/${req.params.gameId}`
   })
-    .then((response) => {
-      res.status(200).send(response.data);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).send('Something went wrong!');
-    });
+    .then((data) => { res.status(200).send(data.data); })
+    .catch(() => { res.status(404).end(); });
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Listening at port:${PORT}`);
 });
