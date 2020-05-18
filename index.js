6f29b3d21c89c48e4a9d366d9156557988febbab
melvinioso <matt.mcgavern@gmail.com>
2020-05-18 01:36:18 -0500

    Deployed proxy setup

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 const app = express();
 const PORT = 3100;
 
-app.use('/games/:gameId', express.static(`${__dirname}/../public`));
+app.use('/games/:gameId', express.static(`${__dirname}/public`));
 app.use(express.json());
 
 
